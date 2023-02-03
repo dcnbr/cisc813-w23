f08fbf0d7064d9d7b34d6fd9d0298abf32736d6f
dcnbr <decembersnb@gmail.com>
2023-02-03 16:27:05 -0500

    Generate objects, start numbers at 1

--- hidato/genhidato.py
+++ hidato/genhidato.py
@@ -8,17 +8,31 @@ preamble = """(define (problem REPLACE)
 print(preamble)
 
 
-# Numbers are consecutive
-symmetric = True
+
+# base objects
+print("\t(:objects")
 total_numbers = grid_width*grid_height
-print("\t\t(are_consecutive n0 n1)")
-for i in range(1,total_numbers-1):
+tile_string = ""
+number_string = ""
+for i in range(0,total_numbers):
+    tile_string = tile_string + "t"+str(i) + " "
+    number_string = number_string + "n"+str(i+1) + " "
+print("\t\t"+tile_string+"- tile")
+print("\t\t"+number_string+"- number")
+
+# end base objects
+print("\t)\n")
+
+# initial predicates
+print("\t(:init")
+
+# consecutive numbers
+symmetric = False
+print("\t\t(are_consecutive n1 n2)")
+for i in range(2,total_numbers):
     print("\t\t(are_consecutive n"+str(i)+" n"+str(i+1)+")")
     if symmetric:
         print("\t\t(are_consecutive n"+str(i)+" n"+str(i-1)+")")
-        
-print("\t\t(are_consecutive n"+str(total_numbers-1)+ " n"+str(total_numbers-2)+")")
-
 
 
 # Generate grid adjacency pairs (symmetric)
